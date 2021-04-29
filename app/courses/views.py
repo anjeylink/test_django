02fc569c437306a5d56904303808ef5eac5bc07f
Andrii Oprysko <anjey.link@gmail.com>
2021-04-29 09:26:35 +0300

    enable search filter for Course

--- app/courses/views.py
+++ app/courses/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from .serializers import CourseSerializer
 from .models import Course
@@ -7,3 +7,5 @@ from .models import Course
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
