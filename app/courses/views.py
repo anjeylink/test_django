db43f2779e13bdea19d3fc1fae1f9576a0a5d450
Andrii Oprysko <anjey.link@gmail.com>
2021-04-29 20:01:56 +0300

    add date filters

--- app/courses/views.py
+++ app/courses/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import CourseSerializer
 from .models import Course
@@ -7,5 +8,6 @@ from .models import Course
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ['name']
+    filterset_fields = ['start_date', 'end_date']
