45528c383599348129c3ecff4d90620cb135f527
Andrii Oprysko <anjey.link@gmail.com>
2021-04-30 10:18:17 +0300

    add link on courses on main page

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Col, Container, Row, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import CoursePage from './components/courses/CoursePage';
 
@@ -11,6 +11,12 @@ const App = () => (
                 <Col>
                     <Switch>
                         <Route path="/courses" component={CoursePage} />
+
+                        <Route path="/">
+                            <div className="p-5">
+                                <Button as={Link} to="/courses">Go to Courses</Button>
+                            </div>
+                        </Route>
                     </Switch>
                 </Col>
             </Row>
