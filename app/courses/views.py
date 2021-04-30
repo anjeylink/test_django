0a9bb7594feaea46bfeb55c5c2b56a379d52262f
Andrii Oprysko <anjey.link@gmail.com>
2021-04-30 10:12:44 +0300

    enable pagination

--- app/courses/views.py
+++ app/courses/views.py
@@ -1,8 +1,14 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, pagination
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import CourseSerializer
 from .models import Course
+
+
+class CoursePagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'perPage'
+    max_page_size = 1000
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -11,3 +17,4 @@ class CourseViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ['name']
     filterset_fields = ['start_date', 'end_date']
+    pagination_class = CoursePagination
